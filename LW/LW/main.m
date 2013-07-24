a1c5be5b725dbb318106bcfdac727d620df7476b
derick.liu <masterliuwei@gmail.com>
2013-07-24 11:13:35 +0800

    init project

---
+++ LW/LW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LW
+//
+//  Created by c5175031 on 7/24/13.
+//  Copyright (c) 2013 DerickLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DLAppDelegate class]));
+    }
+}
